7e0bda788510b47e37efca01f1b9debaf6b1723c
AsifShaikh81 <shaikhasif78866@gmail.com>
2025-08-20 18:38:37 +0530

    20/8/25

--- 4-natours/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('../Server/app') // here importing  'const app = express();'
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on ${port}`);
-});
-//not using this file 
