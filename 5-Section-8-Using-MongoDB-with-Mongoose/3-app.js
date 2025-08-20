f0622701db64de53a888e6e1897f66b5a4c523bb
AsifShaikh81 <shaikhasif78866@gmail.com>
2025-08-20 21:50:33 +0530

    20/8/25

---
+++ 5-Section-8-Using-MongoDB-with-Mongoose/3-app.js
@@ -0,0 +1,46 @@
+//topic:85. Creating a Simple Tour Model
+//*creating express servver
+const express = require('express');
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
+
+// *creating database
+const mongoose = require('mongoose');
+const local_databse = 'mongodb://localhost:27017/learning'; //local databse
+mongoose.connect(local_databse).then(
+  () => console.log('local databse connected'), // connecting database
+);
+
+// *simple way to define scehma
+// const schema1 = new mongoose.Schema({
+//     name: String,
+//     rating: Number,
+//     pricr: Number
+// });
+
+//*Another way to define schema wiht validation
+const schema2 = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: [true, 'error:name cannot be empty'], // validation
+  },
+  rating: {
+    type: Number,
+    default: 4.8, // if there is no value by default it will set 4.8
+  },
+  price: {
+    type: Number,
+    required: [true, 'error:price cannot be empty'], // validation
+  },
+});
+
+// *creating model
+const MODEL = mongoose.model('MODEL', schema2);
+
+//*syntax
+// mongoose.model(<'modelName'>,<schemaName>)
---
+++ 5-Section-8-Using-MongoDB-with-Mongoose/4-app.js
@@ -0,0 +1,64 @@
+//topic:86. Creating Documents and Testing the Model
+
+//*creating express servver
+const express = require('express');
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
+
+// *creating database
+const mongoose = require('mongoose');
+const local_databse = 'mongodb://localhost:27017/learning'; //local databse
+mongoose.connect(local_databse).then(
+  () => console.log('local databse connected'), // connecting database
+);
+
+// *simple way to define scehma
+// const schema1 = new mongoose.Schema({
+//     name: String,
+//     rating: Number,
+//     pricr: Number
+// });
+
+//*Another way to define schema wiht validation
+const schema2 = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'error:name cannot be empty'], // validation
+  },
+  rating: {
+    type: Number,
+    default: 4.8, // if there is no value by default it will set 4.8
+  },
+  price: {
+    type: Number,
+    required: [true, 'error:price cannot be empty'], // validation
+  },
+});
+
+// *creating model
+//*syntax --> mongoose.model(<'modelName'>,<schemaName>)
+const MODEL = mongoose.model('MODEL', schema2);
+
+// -----------------------main topic---------------/
+//*crating document
+//*syntax --> new <collectionName/modelName({...})>
+const DOCUMENT = new MODEL({
+  name: 'Asif',
+  rating: 4.7,
+  price: 445,
+});
+
+//*use for testing purpose ,its optional 
+DOCUMENT.save()
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.log('error:', err);
+  });
+
+//!note : using then() catch() bcz DOCUMENT.save() returns promise
